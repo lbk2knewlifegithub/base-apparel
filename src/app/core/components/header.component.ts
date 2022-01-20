b73f128782abb16721808056b89351f36688e54f
lbk2knewlifegithub <lbk2knewlife@gmail.com>
2022-01-20 23:00:47 +0700

    Fix spacing

--- src/app/core/components/header.component.ts
+++ src/app/core/components/header.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <header class="xl:fixed xl:left-0 xl:top-0 xl:w-full ">
-      <div class="container bg-white py-8 xl:pt-20 xl:bg-transparent">
+      <div class="container bg-white py-8 xl:pt-16 xl:bg-transparent">
         <img
           class="max-w-[140px] xl:max-w-none"
           src="assets/images/logo.svg"
